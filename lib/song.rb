d168e108b41fca881b261e8199a3ef52d50b3324
Michael Carroll <carrollm2@gmail.com>
2019-10-27 01:09:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,16 @@ class Song
     @@all
   end
 
+  def self.create
+    @@all << self.new
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+  end
+
   def save
     self.class.all << self
   end
